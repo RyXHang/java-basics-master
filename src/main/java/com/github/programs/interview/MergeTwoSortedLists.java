6f40990041e22240af4c1fe388174dce7a840069
alanzhang <906849938@qq.com>
2024-07-31 15:06:56 +0800

    modified interview questions

--- src/main/java/com/github/programs/interview/MergeTwoSortedLists.java
+++ src/main/java/com/github/programs/interview/MergeTwoSortedLists.java
@@ -1,2 +1,51 @@
-package com.github.programs.interview;public class MergeTwoSortedLists {
+package com.github.programs.interview;
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0); // 临时头结点
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        // 处理剩余的节点
+        if (l1 != null) {
+            current.next = l1;
+        } else if (l2 != null) {
+            current.next = l2;
+        }
+
+        return dummy.next; // 返回合并后的链表头结点
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+
+        public ListNode getNext() {
+            return this.next;
+        }
+
+        public int getVal() {
+            return this.val;
+        }
+    }
+
 }
--- src/main/java/com/github/programs/interview/PrintDiamond.java
+++ src/main/java/com/github/programs/interview/PrintDiamond.java
@@ -1,2 +1,52 @@
-package com.github.programs.interview;public class PrintDiamond {
+package com.github.programs.interview;
+
+import java.util.Scanner;
+
+public class PrintDiamond {
+    public static void main(String[] args) {
+        // 例子
+        // n = 1
+        //   *
+
+        // n = 2
+        //   *
+        //  * *
+        //   *
+
+        // n = 3
+        //   *
+        //  * *
+        // * * *
+        //  * *
+        //   *
+
+
+        // System.out.println("请输入n的值");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int n = 1;
+
+        for (int i = 1; i <= n; i ++ ) {
+            for (int j = 1; j <= n - i; j ++ ) {
+                System.out.print(" ");
+            }
+            // 打印奇数个"* "即可
+            for (int k = 1; k <= i; k ++ ) {
+                System.out.print("* ");
+            }
+
+            System.out.println(" ");
+        }
+
+        for (int i = 1; i < n ; i ++ ) {
+            for (int j = 0; j < i; j ++ ) {
+                System.out.print(" ");
+            }
+            // 打印奇数个"* "即可
+            for (int k = n - i; k > 0; k -- ) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
